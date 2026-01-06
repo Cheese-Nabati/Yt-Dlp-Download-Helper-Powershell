7702645ff4cc8afe80980ac3c60c6196dd3c0d1a
Dava NabatiKeju <122196728+Cheese-Nabati@users.noreply.github.com>
2026-01-06 22:46:38 +0700

    Re-Directory Files

---
+++ FormatSelections/AudioSelections.ps1
@@ -0,0 +1,22 @@
+﻿Write-Host "`nSelect Audio Quality:" -ForegroundColor Yellow
+Write-Host "[1] High (320kbps)"
+Write-Host "[2] Standart (192kbps)"
+Write-Host "[3] Low (128kbps)"
+Write-Host "[3] Back"
+$vQuality = Read-Host "Select The Quality"
+
+if ($vQuality -eq "3") { . $PSScriptRoot"\ytdlp-interact-host.ps1" }
+
+switch ($vQuality) {
+    "1" { $params += @("-x", "--audio-format", "mp3", "--audio-quality", "320k") }
+    "1" { $params += @("-x", "--audio-format", "mp3", "--audio-quality", "192k") }
+    "1" { $params += @("-x", "--audio-format", "mp3", "--audio-quality", "128k") }
+    Default { $params += @("-f", "bestaudio/best") }
+}
+
+$params += @("--embed-metadata", "--embed-thumbnail")
+$params += @("--ppa", "ExtractAudio: -metadata title='%(title)s' -metadata artist='%(uploader)s'")
+
+Write-Host "`n--- Starting Audio Download ---" -ForegroundColor Green
+& $ytDlpPath $params
+$script:readyToDownload = $true
